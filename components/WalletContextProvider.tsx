87de4dfc7b936cac96f7e0b8be57ab439c108c74
Manza <witleik@gmail.com>
2026-05-11 18:59:29 -0400

    fix: enableWallets true para mostrar Phantom Solflare Backpack en modal

--- components/WalletContextProvider.tsx
+++ components/WalletContextProvider.tsx
@@ -24,7 +24,7 @@ export function WalletContextProvider({ children }: { children: ReactNode }) {
         socials: ["google", "x", "apple"],
         email: false,
       },
-      enableWallets: false,
+      enableWallets: true,
       featuredWalletIds: [
         "a797aa35c0fadbfc1a53e7f675162ed5226968b44a19ee3d24385c64d1d3c393", // Phantom
         "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96", // Solflare
